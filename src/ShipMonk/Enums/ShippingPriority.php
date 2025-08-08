c287ea3bb6ce9675216234c24a5e8e0f0963ca7a
Luis Franco (luigitec) <luigitec@msn.com>
2025-08-08 05:30:04 -0600

    feat: implement ShippingPriority enum for shipping order priorities

---
+++ src/ShipMonk/Enums/ShippingPriority.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ShipMonk\Enums;
+enum ShippingPriority: int
+{
+    case EXPRESS = 1;
+    case PREMIUM = 2;
+    case STANDARD = 3;
+    case ECONOMY = 4;
+
+    public function getLabel(): string
+    {
+        return match ($this) {
+            self::EXPRESS => 'Express',
+            self::PREMIUM => 'Premium',
+            self::STANDARD => 'Standard',
+            self::ECONOMY => 'Economy',
+        };
+    }
+}
